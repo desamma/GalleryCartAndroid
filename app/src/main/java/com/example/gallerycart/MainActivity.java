13cbd1c7b851ed083811d228130c48bb769a1e63
nhanhem2ui <nhanhem2ui@gmail.com>
2025-11-09 14:55:55 +0000

    [FIX]: Fix text and toolbar

--- app/src/main/java/com/example/gallerycart/MainActivity.java
+++ app/src/main/java/com/example/gallerycart/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import com.example.gallerycart.util.SessionManager;
 import com.google.android.material.button.MaterialButton;
 
@@ -22,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         sessionManager = new SessionManager(this);
 
